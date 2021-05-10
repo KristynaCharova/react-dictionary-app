1f35f369d9cb241a894d894a241d37da44dd85e3
KristynaCharova <78442699+KristynaCharova@users.noreply.github.com>
2021-05-10 11:59:18 +0200

    Added basic search engine

---
+++ src/Dictionary.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  const [keyword, setKeyword] = useState("");
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+  function search(event) {
+    event.preventDefault();
+    alert(keyword);
+  }
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
+}
